a9b64f71f18466e81b1358acc8f87fae625921f9
Aaron Watts <aaron.watts@murena.io>
2025-03-24 08:54:16 +0000

    app.js ~+ prevent newline in textarea when return without shift

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -3,10 +3,20 @@
 document.addEventListener('DOMContentLoaded', () => {
     const textarea = document.getElementById("user-input");
 
+    // Prevent return->new line without shift key
+    textarea.addEventListener('keypress', evt => {
+        if (!evt.shiftKey && evt.key == "Enter") {
+            evt.preventDefault();
+            // Do something
+        }
+    });
+
+    // Resize user input to match line number
     textarea.addEventListener("input", () => {
-        textarea.style.height = "auto"; // Reset height
-        textarea.style.height = textarea.scrollHeight + "px"; // Set height based on content
+        textarea.style.height = "auto";
+        textarea.style.height = `${textarea.scrollHeight}px`;
     });
     
+    // Push previous text up (Do again when text added to main)
     window.scrollTo(0, document.body.scrollHeight);
 });
