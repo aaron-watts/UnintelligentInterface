b2c03fc474b247569e6549196b0c6fbabd196973
Aaron Watts <aaron.watts@murena.io>
2025-05-13 10:27:47 +0100

    minor logic changes

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,11 +1,10 @@
-
-
 document.addEventListener('DOMContentLoaded', () => {
     const textarea = document.getElementById("user-input");
 
     // Prevent return->new line without shift key
     textarea.addEventListener('keypress', evt => {
-        if (!evt.shiftKey && evt.key == "Enter") {
+        if (!(window.innerWidth <= 720)
+            && !evt.shiftKey && evt.key == "Enter") {
             evt.preventDefault();
             // Do something
         }
@@ -16,6 +15,11 @@ document.addEventListener('DOMContentLoaded', () => {
         textarea.style.height = "auto";
         textarea.style.height = `${textarea.scrollHeight}px`;
     });
+
+    // Prevent textarea being hidden by virtual keyboard on mobile
+    textarea.addEventListener("focus", () => {
+        window.scrollTo(0, document.body.scrollHeight);
+    })
     
     // Push previous text up (Do again when text added to main)
     window.scrollTo(0, document.body.scrollHeight);
